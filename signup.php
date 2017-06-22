146fe8cce478c01fcfbfb092d9cbbc661e196d9f
Marco Mele <melemarco94@gmail.com>
2017-06-22 16:34:02 +0200

    removed redundant https check

--- signup.php
+++ signup.php
@@ -1,12 +1,5 @@
 <?php
 	session_start();
-	/* HTTPS redirect */
-	if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off') {
-		$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		header("HTTP/1.1 301 Moved permanently");
-		header("Location: " . $redirect);
-		exit();
-	}
 	if(isset($_POST['registrar'])) {
 		include("connect.php");
 		ini_set('display_errors', 1);
